d2197f008bd7e07f74e8e1d238edc5291d1c9a2d
EmanARamadan <emeyadel2009@gmail.com>
2024-05-11 15:18:28 -0400

    update all

--- index.js
+++ index.js
@@ -21,3 +21,17 @@ catch(error){
     display.value = "Error!";
 }
 }
+
+function changeStyle(){
+    document.body.style.backgroundColor = 'green';
+}
+    //document.getElementById("keys").style.color = "yellow";document.getElementById("keys").style.color = "yellow";
+
+    function changeColor() {
+        var colors = document.getElementById("display");
+        colors.style.color = "green";}
+
+    function calcuclator() {
+        var backgroundColor = document.getElementById("calculator")
+        backgroundColor.style.backgroundColor = 'blue';
+    }
